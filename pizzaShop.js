3e39075f6504e7a110970e384af4629e7f2e6303
“yourGithubUsername” <your@emailadd.com>
2021-09-23 15:56:30 +0100

    basis of promise

--- pizzaShop.js
+++ pizzaShop.js
@@ -1,10 +1,29 @@
-const orderPizza = () => new Promise();
-
-// const makePizza
+// create promise
+const orderPizza = () => new Promise(makePizza);
 
 // const eatPizza
+const eatPizza = (customer, pizza) => {
+    console.log(`${customer} says: "Yummy in my tummy. This is my favourite ${pizza}"`)
+}
 
 // const doSomethingWhileWaiting
+const waitingAround = (customer) => {
+    console.log(`${customer} is waiting for their order`)
+};
+
 
 // const complain
+const complain = (issue) => {
+    console.log(`"It is not my fault that ${issue}`)
+};
 
+
+// const makePizza
+let specialPizzaLeft = 4;
+
+const makePizza = (resolve, reject) => {
+    if(specialPizzaLeft > 0) {
+        //makePizza
+        resolve() //send Pizza to customer
+    }
+}
